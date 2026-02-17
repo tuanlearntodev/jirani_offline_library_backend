330e0b05ccbb66fd5b3fe75e8a80249611f95dbc
Sirisha Velavan <sirishakvelavan@gmail.com>
2026-02-17 03:38:00 -0700

    Streaming

--- app/api/video_router.py
+++ app/api/video_router.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, File, UploadFile, Form, Depends
+from fastapi import APIRouter, File, UploadFile, Form, Depends, HTTPException
 from app.schemas.video_schema import Video_Create, Video_View
 from app.repositories.video_repo import Video_Repo
 from sqlalchemy.orm import Session
+from app.models.video import Video
 import os, shutil
+from fastapi.responses import StreamingResponse
 from app.database import get_db
+import uuid
 
 router = APIRouter(prefix="videos", tags=["videos"])
 
@@ -18,7 +21,7 @@ async def upload_file(
 
     upload_directory = "uploads" # folder where uploaded videos will be stored
     os.makedirs(upload_directory, exist_ok=True)
-    file_location = f"{upload_directory}/{file.filename}"
+    file_location = f"{upload_directory}/{uuid.uuid4()}_{file.filename}"
     with open(file_location, "wb") as f:
         shutil.copyfileobj(file.file, f)
 
@@ -39,5 +42,22 @@ async def upload_file(
         title = video_db.title,
         description = video_db.description,
         video_url = video_url,
-        video_type = video_db.video_typed
+        video_type = video_db.video_type
     )
+
+@router.get("/stream/{video_id}")
+def stream_video(video_id: int, db: Session = Depends(get_db)):
+    video = db.query(Video).filter(Video.id == video_id).first()
+    if not video:
+        raise HTTPException(status_code=404, detail="Video not found")
+    
+    def iterfile():
+        with open(video.file_path, "rb") as f: #read file in binary mode = necessary for videos
+            while True: 
+                chunk = f.read(1024*1024) # this is 1MB
+                if not chunk: 
+                    break
+                yield chunk
+
+    return StreamingResponse(iterfile(), media_type=f"video/{video.video_type}")
+
